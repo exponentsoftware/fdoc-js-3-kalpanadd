1d30e46ddd41a8fd44ab3f7582e8cd065f1d4ea8
kalpanadd <dkdevi.dkd@gmail.com>
2021-09-21 19:11:53 +0530

    2A)a done

---
+++ index.js
@@ -0,0 +1,70 @@
+// a. Create a function called ***signUp*** which allows user to add to the collection. If user exists, inform the user that he has already an account.
+
+newuser = {
+    _id: "fg12cy",
+    username: "Asab_k",
+    email: "asab_k@asab.com",
+    password: "123456",
+    createdAt: "17/05/2019 9:30 AM",
+    isLoggedIn: true,
+}
+
+const users = [
+    {
+        _id: "ab12ex",
+        username: "Alex",
+        email: "alex@alex.com",
+        password: "123123",
+        createdAt: "17/05/2019 9:00 AM",
+        isLoggedIn: false,
+    },
+    {
+        _id: "fg12cy",
+        username: "Asab",
+        email: "asab@asab.com",
+        password: "123456",
+        createdAt: "17/05/2019 9:30 AM",
+        isLoggedIn: true,
+    },
+    {
+        _id: "zwf8md",
+        username: "Brook",
+        email: "brook@brook.com",
+        password: "123111",
+        createdAt: "17/05/2019 9:45 AM",
+        isLoggedIn: true,
+    },
+    {
+        _id: "eefamr",
+        username: "Martha",
+        email: "martha@martha.com",
+        password: "123222",
+        createdAt: "17/05/2019 9:50 AM",
+        isLoggedIn: false,
+    },
+    {
+        _id: "ghderc",
+        username: "Thomas",
+        email: "thomas@thomas.com",
+        password: "123333",
+        createdAt: "17/05/2019 10:00 AM",
+        isLoggedIn: false,
+    },
+];
+
+const signup = (new_user) => {
+    let check = 0;
+    for (let user of users) {
+        if (user.username == new_user.username && user.email == new_user.email) {
+            check++;
+            return "User already exists";
+        }
+    }
+    if (check == 0) {
+        users.push(new_user);
+        console.log(users);
+        return "new user added";
+    }
+};
+
+console.log(signup(newuser));
