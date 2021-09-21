0446e584459f781e6baa79318f3dc572222bb072
kalpanadd <dkdevi.dkd@gmail.com>
2021-09-21 19:25:39 +0530

    2A)b solved

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // a. Create a function called ***signUp*** which allows user to add to the collection. If user exists, inform the user that he has already an account.
 
-newuser = {
-    _id: "fg12cy",
-    username: "Asab_k",
-    email: "asab_k@asab.com",
-    password: "123456",
-    createdAt: "17/05/2019 9:30 AM",
-    isLoggedIn: true,
-}
+// newuser = {
+//     _id: "fg12cy",
+//     username: "Asab_k",
+//     email: "asab_k@asab.com",
+//     password: "123456",
+//     createdAt: "17/05/2019 9:30 AM",
+//     isLoggedIn: true,
+// }
 
 const users = [
     {
@@ -52,19 +52,39 @@ const users = [
     },
 ];
 
-const signup = (new_user) => {
-    let check = 0;
+// const signup = (new_user) => {
+//     let check = 0;
+//     for (let user of users) {
+//         if (user.username == new_user.username && user.email == new_user.email) {
+//             check++;
+//             return "User already exists";
+//         }
+//     }
+//     if (check == 0) {
+//         users.push(new_user);
+//         console.log(users);
+//         return "new user added";
+//     }
+// };
+
+// console.log(signup(newuser));
+
+
+
+// b. Create a function called ***signIn*** which allows user to sign in to the application
+
+const signIn = (user_details) => {
     for (let user of users) {
-        if (user.username == new_user.username && user.email == new_user.email) {
-            check++;
-            return "User already exists";
+        if (user.email == user_details.email && user.password == user_details.password) {
+            user.isLoggedIn = true;
+            return "Welcome " + user.username;
         }
     }
-    if (check == 0) {
-        users.push(new_user);
-        console.log(users);
-        return "new user added";
-    }
+    return "Email or Password incorrect";
 };
 
-console.log(signup(newuser));
+const user_details = {
+    email: "alex@alex.com",
+    password: "123123",
+};
+console.log(signIn(user_details));
